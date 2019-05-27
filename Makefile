054c15bf6cb0f9891ddde778775308a750bdc579
EkaterinaRD <katja-ribibna@yandex.ru>
2019-05-27 16:46:58 +0300

    update

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+        javac -sourcepath ./src -d out src/MainClass.java
+        java -classpath ./out MainClass
--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 
     public static void main(String[] args) throws IOException {
 
+        //GetArray tb = new GetArray("test1.txt");
         GetArray tb = new GetArray("test1.txt");
         WhatIsIt result = new WhatIsIt(tb.GetArray(), tb.getLen());
         System.out.println(result.whatIsIt());
